0e2b7d5f6b7e68ec2b632938cbc27f1e0cf5c00d
Sasuoo <116450617+Sasuoo@users.noreply.github.com>
2025-12-04 17:20:39 +0200

    Create User model with schema definition

---
+++ Models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const S = new mongoose.Schema({
+  name: String,
+  phone: String,
+  balance: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model('User', S);
