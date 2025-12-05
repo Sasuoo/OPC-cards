88f22efa2efdb2030ce5e6baa4472abaf674d251
Sasuoo <116450617+Sasuoo@users.noreply.github.com>
2025-12-05 14:48:37 +0200

    Delete Server/db.js

--- Server/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const uri = "mongodb+srv://samioden111_db_user:Clmu5MJXducvqnzp@opccard.7nowyik.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(uri)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("MongoDB error:", err));
-module.exports = mongoose;
