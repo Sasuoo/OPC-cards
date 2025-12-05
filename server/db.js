944c54261dbb0584e7a58ed0be661e93f1d13022
Sasuoo <116450617+Sasuoo@users.noreply.github.com>
2025-12-05 14:59:24 +0200

    Initialize MongoDB connection in db.js

---
+++ server/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const uri = process.env.MONGO_URI;
+
+mongoose.connect(uri)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB error:", err));
+
+module.exports = mongoose;
