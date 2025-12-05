16efc1bfc9f0efa9cc4b27f8089dc31a78569a78
Sasuoo <116450617+Sasuoo@users.noreply.github.com>
2025-12-05 16:43:58 +0200

    Update Product.js

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
-const Schema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  stock: Number,
-  description: String
+
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  category: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true },
+  description: { type: String }
 });
-module.exports = mongoose.model('Product', Schema);
+
+module.exports = mongoose.model('Product', ProductSchema);
